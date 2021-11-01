a0834d1d2dfdd55a874bd821cddc746755695c55
pabhi-pete <pabhi@example.com>
2021-11-01 15:46:16 -0700

    added HTTP verbs for functioning

--- code/app.py
+++ code/app.py
@@ -1,3 +1,4 @@
+from re import L
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_jwt import JWT, jwt_required
@@ -12,7 +13,7 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
-    @jwt_required
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda i: i["name"] == name, items), None) 
         # get object then using next for first item until None no next items
@@ -20,13 +21,28 @@ class Item(Resource):
 
     def post(self, name):
         if next(filter(lambda i: i['name'] == name, items), None):
-            return {"message": "An item with name {!r} already exist".format(name)}, 400
+            return {"message": "An item with name '{}' already exist".format(name)}, 400
 
         data = request.get_json()
-        item = {"name": name, "price": data['price']}
+        item = {"name": name, "price": data["price"]}
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda i: i["name"] != name, items))
+        return {"message": "Item deleted!"}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        if item is None:
+            item = {"name": name, "price": data["price"]}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+        
 class ItemList(Resource):
     def get(self):
         return {"items": items}
--- code/security.py
+++ code/security.py
@@ -10,10 +10,10 @@ userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and safe_str_cmp(user.password,password):
+    if user and safe_str_cmp(user.password, password):
         return user
     
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id,None)
+    return userid_mapping.get(user_id, None)
 
